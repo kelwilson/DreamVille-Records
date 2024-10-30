00305ace956cb18da68a9bcce0d28a2e3c13cc50
Kelwilson <kelwilson_achienu@icloud.com>
2024-10-30 16:54:01 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+
+const dreamVille_artist = {
+    'j cole': {
+        'age': 39,
+        'birthName': 'Jermaine Lamarr Cole',
+        'otherNames': 'Therapist, Hollywood Cole, Kill Edward',
+        'birthdate': '1985', 
+        'birthLocation': 'Frankfurt, West Germany',
+        'origin': 'Fayetteville, North-Carolina',
+        'genre': 'Southern hip hop',
+        'occupation': [ 'Rapper', 'Song writer', 'Record producer', 'Philanthropist', 'Record label manager', 'Singer', 'Basketball player'], 
+        'yearsActive': '2007-present',
+        'labels': ['Dreamville', 'Roc Nation', 'Interscope', 'Columbia',' ByStorm'],
+        'children': 2
+    }, 
+    'bas': {
+        'age': 37,
+        'birthName': 'Abbas Hamad',
+        'otherNames': '',
+        'birthdate': '1987', 
+        'birthLocation': 'Paris, France',
+        'origin': 'New York City, U.S',
+        'genre': 'hip hop',
+        'occupation': [ 'Rapper', 'Song writer'], 
+        'yearsActive': '2010–present',
+        'labels': ['Dreamville', 'Interscope'],
+        'children': null
+    }, 
+    'cozz': {
+        'age': 31,
+        'birthName': 'Cody Rashad Osagie',
+        'otherNames': '',
+        'birthdate': '1993', 
+        'birthLocation': 'Los Angeles, California',
+        'origin': 'Los Angeles, California, U.S',
+        'genre': 'hip hop',
+        'occupation': [ 'Rapper', 'Song writer', 'producer'], 
+        'yearsActive': '2013–present',
+        'labels': ['Dreamville', 'Tha Committee', 'Interscope'],
+        'children': null
+    }, 
+
+}
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+// the colon before the srtistName lets us know what ever comes after the colon is a query parameter
+app.get('/api/:artistName', (req, res) => {
+    //getting the query parameter and saving it in a variable 
+    const artistes_name = req.params.artistName.toLowerCase()
+
+    // checking if the query parameter is a valid artist name
+    if(dreamVille_artist[artistes_name]) {
+            res.json(dreamVille_artist[artistes_name])
+        } else {
+            res.json(dreamVille_artist) 
+    }
+    
+}) 
+
+app.listen(PORT, () => {
+    console.log(`welcome to DREAMVILLE STUDIO where we listen to port ${PORT}`)
+})
